e1590e0b0a52b8279c19fe637069fcf064b5701c
Ezzaldeen <azooz151161@gmail.com>
2025-09-21 00:23:10 +0300

    fix an error

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { ModeToggle } from "@/components/ModeToggle";
 import { ThemeProvider } from "@/providers/theme-provider";
 import { ClerkProvider } from "@clerk/nextjs";
 import Nav from "@/components/Nav";
